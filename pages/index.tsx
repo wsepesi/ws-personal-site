b05a62f18e615633422a8a38bafc976bf9dd52ee
William Sepesi <wsepesi@Williams-MacBook-Air.local>
2023-03-22 11:01:56 +0100

    william [dot] computer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center h-screen bg-[#efdcb18a]">
       <h2 className="text-xl font-text">Hello</h2>
       <text className='italic text-xs m-5 font-text'>Watch this space</text>
     </div>
