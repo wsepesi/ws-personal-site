00fcc828688bf65a686998dd1bf656665e2aedee
William Sepesi <wsepesi@Williams-MacBook-Air.local>
2023-03-22 11:10:06 +0100

    fixed title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-[#efdcb18a]">
-      <h2 className="text-xl font-text">Hello</h2>
-      <text className='italic text-xs m-5 font-text'>Watch this space</text>
-    </div>
+    <>
+      <Head>
+        <title>William [dot] Computer</title>
+      </Head>
+      <div className="flex flex-col justify-center items-center h-screen bg-[#efdcb18a]">
+        <h2 className="text-xl font-text">Hello</h2>
+        <text className='italic text-xs m-5 font-text'>Watch this space</text>
+      </div>
+    </>
   )
 }
